8a7647e23b1f3c9aad01dbd55ae3fbccf46f2a09
Alexandre Gros <xelaflash@gmail.com>
2018-04-13 11:56:09 -0400

    styling for index page, add requirment name photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :ingredients, through: :doses
 
   validates :name, uniqueness: true, presence: true
+  validates :picture, presence: true
 
   mount_uploader :picture, PhotoUploader
 end
