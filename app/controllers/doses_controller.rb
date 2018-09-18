1fe54b7b4b8b29efc2b1987de99be22f58325725
Xelaflash <xelaflash@gmail.com>
2018-09-18 12:30:55 -0400

    spiner ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -4,6 +4,7 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose.cocktail = @cocktail
+
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
