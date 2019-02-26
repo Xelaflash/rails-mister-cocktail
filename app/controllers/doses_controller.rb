2a679954708ddb110bfd30cfaeb7b7457998fab5
Xelaflash <xelaflash@gmail.com>
2019-02-26 09:07:46 -0400

    bug bug bug

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,10 @@
 class DosesController < ApplicationController
 
   def create
-    @dose = Dose.new(dose_params)
-    @cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.cocktail = @cocktail
 
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -14,10 +14,10 @@ class DosesController < ApplicationController
   end
 
   def destroy
+    raise
     @dose = Dose.find(params[:id])
-    @cocktail = @dose.cocktail
     @dose.destroy
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,6 @@ class Dose < ApplicationRecord
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, presence: true
   validates :ingredient, presence: true
   validates :cocktail, uniqueness: { scope: :ingredient }
 end
