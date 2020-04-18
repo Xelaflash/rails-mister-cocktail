157aaf82fac1a78a24ccead5b78748158a668c3a
Alexandre Gros <xelaflash@gmail.com>
2020-04-18 11:14:33 -0400

    set up active admin all done

--- app/admin/cocktails.rb
+++ app/admin/cocktails.rb
@@ -10,4 +10,22 @@ ActiveAdmin.register Cocktail do
     column :created_at
     actions
   end
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :recipe
+      row :photo do |cocktail|
+        image_tag url_for(cocktail.photo), width: 150
+      end
+    end
+  end
+  form do |f|
+    f.inputs 'Cocktail Details' do
+      f.input :name
+      f.input :recipe
+      f.input :photo, as: :file
+    end
+    f.actions
+  end
 end
