e4735ec160f3f1c2be0589fb0bbf865234e7834e
Alexandre Gros <xelaflash@gmail.com>
2020-05-08 11:04:53 -0400

    fix host pb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
- config.action_mailer.default_url_options = { host: "fancy-cocktails2.herokuapp.com" }
+ config.action_mailer.default_url_options = { host: 'fancy-cocktails2.herokuapp.com' }
 
 
   # Code is not reloaded between requests.
